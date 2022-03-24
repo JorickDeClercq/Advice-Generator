148036df8f7a76e736098e2387825d9348320004
Jorick De Clercq <jorick.de.clercq@student.ehb.be>
2022-03-24 19:50:27 +0100

    JS added

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('click', function(e) {
+    // Check if the button was clicked
+    if(!event.target.matches('.button')) return;
+
+    console.log('Button was clicked!')
+
+    fetch("https://api.adviceslip.com/advice")
+        .then((response) => response.json())
+        .then((data) => renderAdvice(data))
+        .catch(() => renderError());
+})
+
+function renderAdvice(data) {
+    const adviceId = document.getElementById("advice__id");
+    const adviceText = document.getElementById("advice__text");
+
+    adviceId.innerHTML = data.slip.id;
+    adviceText.innerHTML = data.slip.advice;
+}
+
+function renderError() {
+    alert("Whoops, something went wrong. Please try again later!");
+}
